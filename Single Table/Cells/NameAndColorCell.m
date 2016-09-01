a65494c38f40c5bcb7ec73c226a4496166ff44d3
Nigel Lee <lihui8@le.com>
2016-09-01 15:01:59 +0800

    develop a single table view with our customizing cell

--- Single Table/Cells/NameAndColorCell.m
+++ Single Table/Cells/NameAndColorCell.m
@@ -28,35 +28,6 @@
     // Configure the view for the selected state
 }
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-        CGRect nameLabelRect = CGRectMake(0, 5, 70, 15);
-        UILabel *nameMarker = [[UILabel alloc] initWithFrame:nameLabelRect];
-        nameMarker.textAlignment = NSTextAlignmentRight;
-        nameMarker.text = @"Name:";
-        nameMarker.font = [UIFont boldSystemFontOfSize:12.f];
-        [self.contentView addSubview:nameMarker];
-        
-        CGRect colourLabelRect = CGRectMake(0, 26, 70, 15);
-        UILabel *colourMarker = [[UILabel alloc] initWithFrame:colourLabelRect];
-        colourMarker.textAlignment = NSTextAlignmentRight;
-        colourMarker.text = @"Colour:";
-        colourMarker.font = [UIFont boldSystemFontOfSize:12.f];
-        [self.contentView addSubview:colourMarker];
-        
-        CGRect nameValueRect = CGRectMake(80, 5, 200, 15);
-        _nameLabel = [[UILabel alloc] initWithFrame:nameValueRect];
-        [self.contentView addSubview:_nameLabel];
-        
-        CGRect colourValueRect = CGRectMake(80, 5, 200, 15);
-        _colourLabel = [[UILabel alloc] initWithFrame:colourValueRect];
-        [self.contentView addSubview:_colourLabel];
-    }
-    return self;
-}
-
 - (void)setName:(NSString *)name {
     if (![name isEqualToString:_name]) {
         _name = [name copy];
@@ -70,43 +41,5 @@
         self.colourLabel.text = _colour;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 @end
--- Single Table/ViewController.m
+++ Single Table/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "NameAndColorCell.h"
 
-static NSString *CellTableIdentifier = @"CellTableIdentifier";
+static NSString *cellTableIdentifier = @"CellTableIdentifier";
 
 @interface ViewController ()
 @property (copy, nonatomic) NSArray *dwarves;
@@ -23,24 +23,18 @@ static NSString *CellTableIdentifier = @"CellTableIdentifier";
     // Do any additional setup after loading the view, typically from a nib.
     
     // in real industry environment, the dwarves likely comes from Server by means of JSON or ProtoBuff(PB)
-//    self.dwarves = @[@"Sleepy", @"Sneezy", @"Bashful", @"Happy",
-//                                    @"Doc", @"Grumpy", @"Dopey",
-//                                    @"Thorin", @"Dorin", @"Nori", @"Ori",
-//                                    @"Balin", @"Dwalin", @"Fili", @"Kili",
-//                                    @"Oin", @"Gloin", @"Bifur", @"Bofur",
-//                                    @"Bombur"];
-//    UITableView *tableView = (id)[self.view viewWithTag:1];
-//    UIEdgeInsets contentInset = tableView.contentInset;
-//    contentInset.top = 20;
-//    [tableView setContentInset:contentInset];
-    self.computers = @[@{@"Name":@"MacBook Air", @"Color":@"Silver"},
-                        @{@"Name":@"MacBook Pro", @"Color":@"Silver"},
-                        @{@"Name":@"iMac", @"Color":@"Silver"},
-                        @{@"Name":@"Mac Mini", @"Color":@"Silver"},
-                        @{@"Name":@"Mac Pro", @"Color":@"Black"}
-                        ];
+    self.computers = @[@{@"Name" : @"MacBook Air", @"Color" : @"Silver"},
+                       @{@"Name" : @"MacBook Pro", @"Color" : @"Silver"},
+                       @{@"Name" : @"iMac", @"Color" : @"Silver"},
+                       @{@"Name" : @"Mac Mini", @"Color" : @"Silver"},
+                       @{@"Name" : @"Mac Pro", @"Color" : @"Black"}];
     UITableView *tableView = (id)[self.view viewWithTag:1];
-    [tableView registerClass:[NameAndColorCell class] forCellReuseIdentifier:CellTableIdentifier];
+    
+    tableView.rowHeight = 65;
+    UINib *nib = [UINib nibWithNibName:@"NameAndColorCell" bundle:nil];
+    
+    [tableView registerNib:nib forCellReuseIdentifier:cellTableIdentifier];
+
     
     UIEdgeInsets contentInset = tableView.contentInset;
     contentInset.top = 20;
@@ -56,71 +50,23 @@ static NSString *CellTableIdentifier = @"CellTableIdentifier";
 
 #pragma mark -dataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//    return [self.dwarves count];
     return [self.computers count];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-//    static NSString *SimpleTableIdentifier = @"SimpleTableIndentifier";
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SimpleTableIdentifier];
-//    
-//    if (nil == cell) {
-//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:SimpleTableIdentifier];
-//    }
-//    
-//    UIImage *image = [UIImage imageNamed:@"star"];
-//    cell.imageView.image = image;
-//    
-//    cell.textLabel.text = self.dwarves[indexPath.row];
-//    if (indexPath.row < 7) {
-//        cell.detailTextLabel.text = @"Mr. Disney";
-//    } else {
-//        cell.detailTextLabel.text = @"Mr. Tolkien";
-//    }
-//    
-//    cell.textLabel.font = [UIFont boldSystemFontOfSize:20.f];
-//    
-//    return cell;
-    NameAndColorCell *cell = [tableView dequeueReusableCellWithIdentifier:CellTableIdentifier forIndexPath:indexPath];
+    NameAndColorCell *cell = [tableView dequeueReusableCellWithIdentifier:cellTableIdentifier
+                                                                forIndexPath:indexPath];
+    
     NSDictionary *rowData = self.computers[indexPath.row];
+    
     cell.name = rowData[@"Name"];
-    cell.colour = rowData[@"Colour"];
+    cell.colour = rowData[@"Color"];
     
     return cell;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView
-indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return indexPath.row;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    return 50.f;
-    return 0.f;
-}
-
 #pragma mark -delegate
-- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-//    if (indexPath.row == 0) {
-//        return nil;
-//    } else {
-//        return indexPath;
-//    }
-    return nil;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-//    NSString *rowValue = [[NSString alloc] initWithFormat:@"%@", self.dwarves[indexPath.row]];
-//    NSString *message = [[NSString alloc] initWithFormat:@"You selected: %@", rowValue];
-//    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Row Selected!" message:message preferredStyle:UIAlertControllerStyleAlert
-//                                ];
-//    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Yes, I Did."
-//                                                     style:UIAlertActionStyleDefault
-//                                                   handler:nil
-//                             ];
-//    [alert addAction:action];
-//    [self presentViewController:alert animated:YES completion:nil];
-}
 
 @end
